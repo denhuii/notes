a6ed495841026d5e8f13620cc18db44289d4f727
H <873023670@qq.com>
2020-05-27 23:07:29 +0800

    修改样式

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,18 @@
 module.exports = {
+  // 部署站点的基础路径
   base: '/notes/',
+  title: '个人笔记',
   themeConfig: {
     // 你的GitHub仓库，请正确填写
-    // repo: 'https://github.com/xxxxxxx/blog-demo',
+    repo: 'https://github.com/denhuii/notes',
     // 自定义仓库链接文字。
-    repoLabel: 'My GitHub',
+    repoLabel: 'GitHub',
+    // 导航栏
     nav: [
       { text: 'Home', link: '/' },
       { text: 'FirstBlog', link: '/blog/FirstBlog.md' },
     ],
+    // 侧边栏
     sidebar: [
       ['/', '首页'],
       ['/blog/FirstBlog.md', '我的第一篇牛逼博客'],
